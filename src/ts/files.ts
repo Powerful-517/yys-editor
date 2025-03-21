08bb1bdb7fd4f3995fe3f8f38fc2e4ea25c758d2
rookie4show <rookie4show@gmail.com>
2025-03-21 16:04:22 +0800

    更新公告，水印保存，数据保存提示优化

--- src/ts/files.ts
+++ src/ts/files.ts
@@ -4,12 +4,33 @@ import {useGlobalMessage} from "./useGlobalMessage";
 
 const { showMessage } = useGlobalMessage();
 
+function getDefaultState() {
+    return {
+        fileList: [{
+            "label": "File 1",
+            "name": "1",
+            "visible": true,
+            "type":"PVE",
+            "groups": [
+                {
+                    "shortDescription": "",
+                    "groupInfo": [
+                        {}, {}, {}, {}, {}
+                    ],
+                    "details": ""
+                }
+            ]
+        }],
+        activeFile: "1",
+    };
+}
+
 function saveStateToLocalStorage(state) {
     localStorage.setItem('filesStore', JSON.stringify(state));
 }
 
-function clearLocalStorage() {
-    localStorage.clear()
+function clearFilesStoreLocalStorage() {
+    localStorage.removeItem('filesStore')
 }
 
 function loadStateFromLocalStorage() {
@@ -17,199 +38,17 @@ function loadStateFromLocalStorage() {
 }
 
 export const useFilesStore = defineStore('files', {
-    state: () => ({
-        fileList: [
-            {
-                "label": "Welcome",
-                "name": "1",
-                "type":"PVE",
-                "visible": true,
-                "groups": [
-                    {
-                        "shortDescription": "<h1>鬼灵歌姬</h1><h2><em>这是一个演示项目，用于测试显示6个式神的对齐效果</em></h2>",
-                        "groupInfo": [
-                            {
-                                "avatar": "/assets/Shikigami/sp/372.png",
-                                "name": "因幡辉夜姬",
-                                "rarity": "SP"
-                            },
-                            {
-                                "avatar": "/assets/Shikigami/ssr/356.png",
-                                "name": "千姬",
-                                "rarity": "SSR"
-                            },
-                            {
-                                "avatar": "/assets/Shikigami/sp/554.png",
-                                "name": "纺愿缘结神",
-                                "rarity": "SP"
-                            },
-                            {
-                                "avatar": "/assets/Shikigami/ssr/556.png",
-                                "name": "天照",
-                                "rarity": "SSR"
-                            },
-                            {
-                                "avatar": "/assets/Shikigami/ssr/557.png",
-                                "name": "伊邪那美",
-                                "rarity": "SSR"
-                            },
-                            {
-                                "avatar": "/assets/Shikigami/sp/367.png",
-                                "name": "绘世花鸟卷",
-                                "rarity": "SP"
-                            }
-                        ],
-                        "details": "<h2><strong>开局因幡普攻主怪后锁二</strong></h2>"
-                    },
-                    {
-                        "shortDescription": "<h1>魂土15秒</h1><p><em>相同式神编辑不同属性</em></p>",
-                        "groupInfo": [
-                            {
-                                "avatar": "/assets/Shikigami/ssr/364.png",
-                                "name": "阿修罗",
-                                "rarity": "SSR",
-                                "properties": {
-                                    "edit": true,
-                                    "yuhun": {
-                                        "yuhunSetEffect": [
-                                            {
-                                                "name": "狂骨",
-                                                "shortName": "狂",
-                                                "type": "attack",
-                                                "avatar": "/assets/Yuhun/狂骨.png"
-                                            },
-                                            {
-                                                "name": "荒骷髅",
-                                                "shortName": "荒",
-                                                "type": "PVE",
-                                                "avatar": "/assets/Yuhun/荒骷髅.png"
-                                            }
-                                        ],
-                                        "target": "1",
-                                        "property2": [
-                                            "Attack"
-                                        ],
-                                        "property4": [
-                                            "Attack"
-                                        ],
-                                        "property6": [
-                                            "Crit",
-                                            "CritDamage"
-                                        ]
-                                    },
-                                    "levelRequired": "40",
-                                    "speed": "",
-                                    "skillRequiredMode": "all",
-                                    "skillRequired": [
-                                        "5",
-                                        "5",
-                                        "5"
-                                    ]
-                                }
-                            },
-                            {
-                                "avatar": "/assets/Shikigami/ssr/364.png",
-                                "name": "阿修罗",
-                                "rarity": "SSR",
-                                "properties": {
-                                    "edit": true,
-                                    "yuhun": {
-                                        "yuhunSetEffect": [
-                                            {
-                                                "name": "狂骨",
-                                                "shortName": "狂",
-                                                "type": "attack",
-                                                "avatar": "/assets/Yuhun/狂骨.png"
-                                            },
-                                            {
-                                                "name": "鬼灵歌伎",
-                                                "shortName": "歌伎",
-                                                "type": "PVE",
-                                                "avatar": "/assets/Yuhun/鬼灵歌伎.png"
-                                            }
-                                        ],
-                                        "target": "0",
-                                        "property2": [
-                                            "Attack"
-                                        ],
-                                        "property4": [
-                                            "Attack"
-                                        ],
-                                        "property6": [
-                                            "Crit",
-                                            "CritDamage"
-                                        ]
-                                    },
-                                    "levelRequired": "40",
-                                    "speed": "",
-                                    "skillRequiredMode": "all",
-                                    "skillRequired": [
-                                        "5",
-                                        "5",
-                                        "5"
-                                    ]
-                                }
-                            },
-                            {
-                                "avatar": "/assets/Shikigami/ssr/370.png",
-                                "name": "饭笥",
-                                "rarity": "SSR"
-                            },
-                            {
-                                "avatar": "/assets/Shikigami/ssr/369.png",
-                                "name": "食灵",
-                                "rarity": "SSR"
-                            },
-                            {
-                                "avatar": "/assets/Shikigami/r/205.png",
-                                "name": "座敷童子",
-                                "rarity": "R"
-                            }
-                        ],
-                        "details": ""
-                    }
-                ]
-            },
-            {
-                "label": "Test",
-                "name": "2",
-                "visible": true,
-                "type":"PVE",
-                "groups": [
-                    {
-                        "shortDescription": "<h1>御魂·悲鸣</h1><p>这是一个演示项目，用于测试不同标签页的切换效果</p>",
-                        "groupInfo": [
-                            {},
-                            {},
-                            {},
-                            {},
-                            {}
-                        ],
-                        "details": ""
-                    },
-                    {
-                        "shortDescription": "",
-                        "groupInfo": [
-                            {},
-                            {},
-                            {},
-                            {},
-                            {}
-                        ],
-                        "details": ""
-                    }
-                ]
-            }
-        ],
-        activeFile: "1",
-    }),
+    state: () => getDefaultState(),
     getters: {
         visibleFiles: (state) => state.fileList.filter(file => file.visible),
     },
     actions: {
         initializeWithPrompt() {
             const savedState = loadStateFromLocalStorage();
-            if (savedState) {
+            const defaultState = getDefaultState();
+
+            const isSame = JSON.stringify(savedState) === JSON.stringify(defaultState);
+            if (savedState && !isSame) {
                 ElMessageBox.confirm(
                     '检测到有未保存的旧数据，是否恢复？',
                     '提示',
@@ -223,7 +62,7 @@ export const useFilesStore = defineStore('files', {
                     this.activeFile = savedState.activeFile || "1";
                     showMessage('success', '数据已恢复');
                 }).catch(() => {
-                    clearLocalStorage();
+                    clearFilesStoreLocalStorage();
                     showMessage('info', '选择了不恢复旧数据');
                 });
             }
